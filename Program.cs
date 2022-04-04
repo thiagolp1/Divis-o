79c2eedcea2307350efaee65c8faaabaecf44d73
Thiago <Thiagolopespereiralp1@gmail.com>
2022-04-04 20:04:08 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿double numerador, denominador, quociente;
+
+Console.Write("Digite o numerador......: ");
+numerador = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o denominador....: ");
+denominador = Convert.ToDouble(Console.ReadLine());
+
+if (denominador == 0)
+{
+       Console.WriteLine("Não é possível dividir por zero.");
+}
+
+else 
+{
+     quociente = numerador / denominador;
+     Console.WriteLine($"{numerador:N1} dividido por {denominador:N1} é {quociente:N1}.");
+}
